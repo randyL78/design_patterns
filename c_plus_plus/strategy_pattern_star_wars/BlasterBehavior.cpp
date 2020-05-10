f6ac147e313ce301aadad75e92251ec16d82a00f
Randy Layne <randydavidl78@gmail.com>
2020-05-10 19:22:38 -0400

    A different strategy pattern implementation

---
+++ c_plus_plus/strategy_pattern_star_wars/BlasterBehavior.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Randy Layne on 5/10/20.
+//
+
+#include <iostream>
+#include "BlasterBehavior.h"
+
+void BlasterBehavior::useWeapon() {
+    std::cout << "I am using my blaster!\n";
+}
---
+++ c_plus_plus/strategy_pattern_star_wars/LightSaberBehavior.cpp
@@ -0,0 +1,10 @@
+//
+// Created by Randy Layne on 5/10/20.
+//
+#include <iostream>
+
+#include "LightSaberBehavior.h"
+
+void LightSaberBehavior::useWeapon() {
+    std::cout << "I am using my light saber!\n";
+}
